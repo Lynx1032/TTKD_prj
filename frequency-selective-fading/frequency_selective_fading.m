d6067783be8433b6ad2afcde1669850f09ff0e48
Lynx1032 <71201272+Lynx1032@users.noreply.github.com>
2023-08-25 18:18:37 +0700

    Add files via upload

--- frequency-selective-fading/frequency_selective_fading.m
+++ frequency-selective-fading/frequency_selective_fading.m
@@ -1,43 +1,87 @@
-% Simulation parameters
-numSymbols = 100;              % Number of symbols to transmit
-numTaps = 5;                    % Number of channel taps (multipath components)
-snr = -2.5;                       % Signal-to-Noise Ratio in dB
+% Thông số mô phỏng
+numSymbols = 100;      % Số lượng ký hiệu - mẫu thử
+numSubcarriers = 50;    % Số lượng sóng mang con
+symbolRate = 10^2;       % Tốc độ truyền ký hiệu - tính bằng Hz
+channelBandwidth = 10^3; % Băng thông kênh truyền - tính bằng Hz
 
-% Generate random symbols
-symbols = 2 * randi([0 1], numSymbols, 1) - 1;  % BPSK symbols (-1 and +1)
+% Khởi tạo nguồn dữ liệu truyền - mỗi sóng mang con sẽ truyền số ký hiệu = tống ký hiệu/số sóng mang
+bits = randi([0, 1], numSubcarriers, numSymbols/numSubcarriers);
+plt_bits = reshape(transpose(bits), [1, numSymbols]);
 
-% Generate channel taps (complex Gaussian)
-channelTaps = (randn(numTaps, 1) + 1i * randn(numTaps, 1)) / sqrt(2);
-
-% Create frequency-selective fading channel
-channelFilter = zeros(numSymbols, numTaps);
-for i = 1:numTaps
-    channelFilter(i:numSymbols, i) = symbols(1:numSymbols-i+1);
+% Điều chế BPSK
+symbols = zeros(numSubcarriers, numSymbols/numSubcarriers);
+for i = 1:numSubcarriers
+    symbols(i, :) = 2*(bits(i, :)) - 1;
 end
 
-% Apply channel fading
-fadedSymbols = channelFilter * channelTaps;
+% Khởi tạo kênh
+fadingCoefficients = (randn(numSubcarriers, numSymbols/numSubcarriers) + 1i * randn(numSubcarriers, numSymbols/numSubcarriers)) / sqrt(2);
 
-% Add noise to the received signal
-noise = sqrt(0.5 / (10^(snr/10))) * (randn(numSymbols, 1) + 1i * randn(numSymbols, 1));
-receivedSymbols = fadedSymbols + noise;
+% Khởi tạo đáp ứng tần số cho kênh
+frequencyResponse = zeros(numSubcarriers, numSymbols/numSubcarriers);
+for i = 1:numSubcarriers
+    magnitude = rand(1, numSymbols/numSubcarriers);
+    phase = 2 * pi * rand(1, numSymbols/numSubcarriers);
+    frequencyResponse(i, :) = magnitude .* exp(1i * phase);
+end
 
-% Calculate channel magnitude
-channelMagnitude = abs(channelTaps);
+% Thêm kênh truyền vào đường truyền tín hiệu
+receivedSymbols = zeros(numSubcarriers, numSymbols/numSubcarriers);
+for i = 1:numSymbols/numSubcarriers
+    receivedSymbols(:, i) = symbols(:, i) .* frequencyResponse(:, i) .* fadingCoefficients(:, i);
+end
 
-% Calculate received signal amplitude
-receivedAmplitude = abs(receivedSymbols);
+% Giải điều chế BPSK
+receivedSymbols_ts = reshape(transpose(receivedSymbols), [1, numSymbols]);
+receivedSymbols_demodulation = real(receivedSymbols_ts) > 0;
 
-% Plot channel magnitude
+% Tính tỉ lệ lỗi bit (BER)
+numErrors = sum(plt_bits ~= receivedSymbols_demodulation);
+ber = numErrors / numSymbols;
+
+% Vẽ đồ thị đáp ứng
 figure;
-stem(1:numTaps, channelMagnitude, 'b', 'LineWidth', 1.5);
-xlabel('Tap Index');
-ylabel('Magnitude');
-title('Channel Magnitude');
+subplot(2, 1, 1);
+plot(1:numSubcarriers, abs(frequencyResponse(:, 1)));
+title('Đáp ứng độ lớn kênh');
+xlabel('Số sóng mang con');
+ylabel('Độ lớn của kênh');
+grid on;
 
-% Plot received signal amplitude
+subplot(2, 1, 2);
+plot(1:numSubcarriers, angle(frequencyResponse(:, 1)));
+title('Đáp ứng pha kênh');
+xlabel('Số sóng mang con');
+ylabel('Pha của kênh');
+grid on;
+
+% Plot the received symbols
 figure;
-plot(1:numSymbols, 10*log10(receivedSymbols.^2), 'r', 'LineWidth', 1.5);
-xlabel('Symbol Index');
-ylabel('Amplitude');
-title('Received Signal Amplitude');
+plot(1:numSubcarriers, 10*log10(abs(real(receivedSymbols(:, 1)).^2)), 'b');
+hold on;
+plot(1:numSubcarriers, 10*log10(abs(imag(receivedSymbols(:, 1)).^2)), 'r');
+xlabel('Số sóng mang con');
+ylabel('Cường đô tín hiệu nhận tại đầu thu (dB)');
+title('Cường độ tín hiệu tại đầu thu');
+legend('Phần thực','Phần ảo');
+hold off;
+grid on;
+
+% Vẽ đồ thị tín hiệu truyền, nhận và BER
+figure;
+t = 1:numSymbols;
+
+subplot(2,1,1);
+stem(t, plt_bits, 'b')
+title('Tín hiệu truyền');
+xlabel('Mẫu kí hiệu');
+ylabel('Giá trị');
+hold on;
+
+subplot(2,1,2);
+stem(t, receivedSymbols_demodulation, 'r');
+title('Tín hiệu nhận sau giải điều chế');
+xlabel('Mẫu kí hiệu');
+ylabel('Giá trị');
+text(0.45, 1.3, ['BER: ' num2str(ber)], 'Units', 'normalized', 'Fontsize', 22);
+hold off;
---
+++ slow-fading/slow_fading.m
@@ -0,0 +1,40 @@
+% Các thông số mô phỏng
+numSamples = 100;        % Số mẫu thử
+samplingRate = 1e6;      % tốc độ lấy mẫu - Hz
+carrierFreq = 2.4e9;     % Tần số sóng mang - Hz
+velocity = 33.33;         % Tốc độ di chuyển của trạm thu m/s
+pathLossExponent = 2; % hệ số path loss 
+shadowingStdDev = 4;  % Phương sai shadowing chuẩn
+fadingStdDev = 1;       % phương sai fading chuẩn
+
+% tạo mẫu kênh slow-fading
+t = (0:numSamples-1) / samplingRate;
+fd = velocity / carrierFreq;              % tần số Doppler tối đa
+tau = 1 / fd;                            % Độ trễ trải tối đa
+pathGain = sqrt(1/2) * (randn(numSamples, 1) + 1i * randn(numSamples, 1)); % guassian phức rayleigh
+shadowing = db2mag(shadowingStdDev * randn(numSamples, 1));  % độ lợi shadowing
+fading = db2mag(fadingStdDev * randn(numSamples, 1));        % độ lợi fading
+pathLoss = (carrierFreq / (4*pi)) .^ pathLossExponent;        % Path loss
+
+% Nội suy độ lợi và khớp với trục thời gian
+interpPathGain = interp1([0, tau], [1, 1], t, 'linear', 'extrap');
+
+% Tính toán việc lấy mẫu của kênh
+channelSamples = sqrt(pathLoss) .* shadowing .* interpPathGain .* fading;
+
+% Đồ thị đáp ứng xung
+impulseResponse = ifft(pathGain);
+timeAxis = (0:length(impulseResponse)-1) / samplingRate;
+figure;
+plot(timeAxis, 10*log10(impulseResponse));
+xlabel('Thời gian (s)');
+ylabel('Biên độ (dB)');
+title('Đáp ứng xung kênh truyền');
+
+% Đồ thị lấy mẫu kênh
+timeAxis = (0:numSamples-1) / samplingRate;
+figure;
+plot(timeAxis, abs(channelSamples).^2);
+xlabel('Thời gain (s)');
+ylabel('Độ lớn');
+title('Lấy mẫu kênh');
